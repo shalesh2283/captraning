27b7606ffa6397f50d0c7904a1a2d8f1284aceef
Shalesh Thakur <shalesh2283.be22@chitkara.edu.in>
2026-01-02 09:28:47 +0530

    1-1-26

---
+++ src/level1/one.java
@@ -0,0 +1,13 @@
+package level1;
+
+public class one {
+    public static void main(String[] args) {
+
+        int birthYear = 2000;
+        int currentYear = 2024;
+
+        int age = currentYear - birthYear;
+
+        System.out.println("Harry's age in 2024 is " + age);
+    }
+}
---
+++ src/level1/third.java
@@ -0,0 +1,11 @@
+package level1;
+
+public class third {
+    public static void main(String[] args) {
+
+        double kilometers = 10.8;
+        double miles = kilometers * 1.6;
+
+        System.out.println("The distance " + kilometers + " km in miles is " + miles);
+    }
+}
