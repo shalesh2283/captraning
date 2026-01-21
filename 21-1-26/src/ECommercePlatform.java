2a20703188f4665483774fe053f2c2e9d667df48
Shalesh Thakur <shalesh2283.be22@chitkara.edu.in>
2026-01-21 18:19:52 +0530

    Add work for 21-1-26

---
+++ 21-1-26/src/ECommercePlatform.java
@@ -0,0 +1,57 @@
+interface Taxable {
+    double calculateTax();
+    String getTaxDetails();
+}
+
+abstract class Product {
+    private int productId;
+    private String name;
+    private double price;
+
+    public Product(int id, String name, double price) {
+        this.productId = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public double getPrice() { return price; }
+    public abstract double calculateDiscount();
+}
+
+class Electronics extends Product implements Taxable {
+    public Electronics(int id, String name, double price) {
+        super(id, name, price);
+    }
+
+    public double calculateDiscount() {
+        return getPrice() * 0.1;
+    }
+
+    public double calculateTax() {
+        return getPrice() * 0.18;
+    }
+
+    public String getTaxDetails() {
+        return "18% GST";
+    }
+}
+
+class Clothing extends Product {
+    public Clothing(int id, String name, double price) {
+        super(id, name, price);
+    }
+
+    public double calculateDiscount() {
+        return getPrice() * 0.2;
+    }
+}
+
+class Groceries extends Product {
+    public Groceries(int id, String name, double price) {
+        super(id, name, price);
+    }
+
+    public double calculateDiscount() {
+        return getPrice() * 0.05;
+    }
+}
