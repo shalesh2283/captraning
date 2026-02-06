8c52ba0d3cf6962bb868ab18f85b61d39ebe3759
Shalesh Thakur <shalesh2283.be22@chitkara.edu.in>
2026-02-06 19:04:28 +0530

    Work for 6-2-26

---
+++ 6-2-26/src/DatabaseConnection.java
@@ -0,0 +1,15 @@
+public class DatabaseConnection {
+    boolean connected = false;
+
+    public void connect() {
+        connected = true;
+    }
+
+    public void disconnect() {
+        connected = false;
+    }
+
+    public boolean isConnected() {
+        return connected;
+    }
+}
---
+++ 6-2-26/src/EvenChecker.java
@@ -0,0 +1,5 @@
+public class EvenChecker {
+    public boolean isEven(int n) {
+        return n % 2 == 0;
+    }
+}
